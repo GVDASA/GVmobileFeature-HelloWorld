4e3d7235215adbd8cdf9c2752eb668ea33de1bba
dojbraga <dojbraga@gmail.com>
2015-09-21 18:15:22 -0300

    Adicionando exemplos de parametros para chamada da VIEW

--- helloWorld.controller.js
+++ helloWorld.controller.js
@@ -7,7 +7,21 @@ angular.module('feature.helloWorld')
             navigationService: navigationService
         });
 
-        SampleService.create().query({}, function (data) {
+
+        // Parametros que serão passados para a VIEW
+        var params = {
+                $filter:'codigopessoa eq '+$scope.usuario.codigoPessoa,          // Onde o copdigoPessoa for igual a 42
+                // $filter:'CodigoPessoa gt 1 lt 42 ' // Onde o codigoPessoa for maior que 1 e menor que 42
+                $orderby:'codigopessoa'               // Ordenado por codigoPessoa
+                //$top: 10, // Dez registros por página
+                //$skip: 10 // Indica quantos registros devem ser ignorados caso deseje utilizar paginação
+                //$select: 'NOME, NACIONALIDADE, ESTADOCIVIL' // Indica quais campos serão retornados na consulta.
+
+        }
+
+
+        // Chamada do serviço que executa a VIEW
+        SampleService.create().query(params, function (data) {
             console.log(data);
             gvmMessagesService.info('Consulta com sucesso.');
         }, function (data) {
