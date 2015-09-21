b742d48760b1eb9f97ab9932cde2fb22d04dd9f7
dojbraga <dojbraga@gmail.com>
2015-09-21 18:26:49 -0300

    Adicionando comentários de como chamar a VIEW.

--- helloWorld.service.js
+++ helloWorld.service.js
@@ -1,9 +1,14 @@
 angular.module('feature.helloWorld')
 
+
+/* O SampleService está realizando a chamada para a view -- GVMVW_PESSOA
+   Caso queira excecutar outra view por exemplo -- GVMVWM_EXEMPLO a url passada para API deverá ser: "~/api/views/exemplo/"
+*/
+
 .factory('SampleService', function($resource) {
     return {
         create: function() {
-            return $resource("~/api/views/pessoa/?$orderby=nome", {}, {
+            return $resource("~/api/views/pessoa/", {}, {
                 "update": {
                     method: "PUT"
                 },
